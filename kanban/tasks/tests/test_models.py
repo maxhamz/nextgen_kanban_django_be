f17a167551c7a67ae587537c6eca46158ce8dc41
maxhamz <maxwell.hamzah@gmail.com>
2021-03-13 19:52:38 +0700

    finished unittest on models

--- kanban/tasks/tests/test_models.py
+++ kanban/tasks/tests/test_models.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
-from tasks.models import Task
-from django.db import models
 from django.contrib.auth.models import User
-# from .models import Task
+from tasks.models import Task
 from datetime import datetime, timedelta
 
 # THIS IS UNIT TEST
@@ -26,9 +24,10 @@ class TaskModelTest(TestCase):
             # due_date=models.DateTimeField(
             #     datetime.now() + timedelta(days=2)
             # ),
+            due_date=(datetime.now() + timedelta(days=2)),
             title='Test task 1',
             details='Test details goes here',
-            # status='WIP',
+            status=Task.TaskStatus['WIP'],
             owner=user
         )
 
@@ -47,7 +46,17 @@ class TaskModelTest(TestCase):
         field_label = task._meta.get_field('owner').verbose_name
         self.assertEqual(field_label, 'owner')
 
+    def test_status_label(self):
+        task = Task.objects.get(id=1)
+        field_label = task._meta.get_field('status').verbose_name
+        self.assertEqual(field_label, 'status')
+
     def test_created_label(self):
         task = Task.objects.get(id=1)
         field_label = task._meta.get_field('created').verbose_name
         self.assertEqual(field_label, 'created')
+
+    def test_due_date_label(self):
+        task = Task.objects.get(id=1)
+        field_label = task._meta.get_field('due_date').verbose_name
+        self.assertEqual(field_label, 'due date')
